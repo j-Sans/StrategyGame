947f3824121e809835ba16e89c5aa1672217ac5b
theonlycaosun <michaelcaosun@michaels-mbp-2.devices.brown.edu>
2016-06-26 17:10:05 -0400

    finished move creature func

--- StrategyGame/Board.cpp
+++ StrategyGame/Board.cpp
@@ -9,12 +9,26 @@
 #include "Board.hpp"
 
 void Board::moveCreature(unsigned int x, unsigned int y, Direction moveTo) {
-    if (x >= this->gameBoard.size())
+
+    if (x >= this->gameBoard.size()) //is this protection really necessary?
         throw std::range_error("X out of range");
     if (y >= this->gameBoard[0].size())
         throw std::range_error("Y out of range");
     
-    if (moveTo == North) {
-        //...
+    if (moveTo == North) { //graphics and board aren't linked yet, so i'll temporarily set y to north and x to east. I think this should make north the top-left corner of screen?
+        gameBoard[x][y+1].setCreature(gameBoard[x][y].creature());
+        gameBoard[x]y].setCreature(nullptr)
+    } else if (moveTo == East) {
+        gameBoard[x+1][y].setCreature(gameBoard[x][y].creature());
+        gameBoard[x]y].setCreature(nullptr)
+       
+    } else if (moveTo == South) {
+        gameBoard[x][y-1].setCreature(gameBoard[x][y].creature());
+        gameBoard[x]y].setCreature(nullptr)
+        
+    } else if (moveTo == West) {
+        gameBoard[x-1][y].setCreature(gameBoard[x][y].creature());
+        gameBoard[x]y].setCreature(nullptr)
+        
     }
 }
