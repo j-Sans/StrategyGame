e7c876d5ef4a0dc51f645cca8872ede25e308d62
ja-San <jake_sanders@horacemann.org>
2016-07-27 14:47:30 -0400

    Started font class. Unfinished
    Currently doesn’t run, but the font class can be commented out to let
    it run, it isn’t used or implemented at all yet. The problems lie with
    the library being unlinked so far. We should also discuss the library

--- StrategyGame/Font.cpp
+++ StrategyGame/Font.cpp
@@ -5,5 +5,22 @@
 //  Created by Jake Sanders on 7/27/16.
 //  Copyright © 2016 Jake Sanders. All rights reserved.
 //
+//  Guided by the tutorial at http://learnopengl.com/#!In-Practice/Text-Rendering by Joey De Vries
+//  This has been altered from the original code, and is not endoresed nor endorsed by LearnOpenGL
+//
 
 #include "Font.hpp"
+
+Font::Font(const char* fontPath) {
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft))
+        std::cout << "Error initializing FreeType font library" << std::endl;
+    
+    FT_Face face;
+    if (FT_New_Face(ft, fontPath, 0, &face))
+        std::cout << "Error loading font" << std::endl;
+    
+    FT_Set_Pixel_Sizes(face, 0, 48);
+    
+    
+}
--- StrategyGame/Font.hpp
+++ StrategyGame/Font.hpp
@@ -5,10 +5,34 @@
 //  Created by Jake Sanders on 7/27/16.
 //  Copyright © 2016 Jake Sanders. All rights reserved.
 //
+//  Guided by the tutorial at http://learnopengl.com/#!In-Practice/Text-Rendering by Joey De Vries
+//  This has been altered from the original code, and is not endoresed nor endorsed by LearnOpenGL
+//
 
 #ifndef Font_hpp
 #define Font_hpp
 
-#include <stdio.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include <iostream>
+
+class Font {
+public:
+    
+    //Constructor
+    Font(const char* fontPath);
+    
+    //Public properties
+    
+    //Public member functions
+    
+    //Public get functions
+    
+private:
+    //Private properties
+    
+    //Private member functions
+};
 
 #endif /* Font_hpp */
