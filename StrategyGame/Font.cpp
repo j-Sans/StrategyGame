3ee65d7b6d55645fa0de9bab05dd191df9eadeec
ja-San <jake_sanders@horacemann.org>
2016-07-27 14:09:18 -0400

    Going to add fonts

---
+++ StrategyGame/Font.cpp
@@ -0,0 +1,9 @@
+//
+//  Font.cpp
+//  Strategy Game
+//
+//  Created by Jake Sanders on 7/27/16.
+//  Copyright © 2016 Jake Sanders. All rights reserved.
+//
+
+#include "Font.hpp"
---
+++ StrategyGame/Font.hpp
@@ -0,0 +1,14 @@
+//
+//  Font.hpp
+//  Strategy Game
+//
+//  Created by Jake Sanders on 7/27/16.
+//  Copyright © 2016 Jake Sanders. All rights reserved.
+//
+
+#ifndef Font_hpp
+#define Font_hpp
+
+#include <stdio.h>
+
+#endif /* Font_hpp */
