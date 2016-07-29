0a634971b633234e2867e1c174894de2ff6223ab
ja-San <jake_sanders@horacemann.org>
2016-07-29 12:59:40 -0400

    Added LearnOpenGL font code
    Still have to update the code with commenting. Simply added it in then
    fixed any blatant errors (like naming errors) so errors don’t appear

--- StrategyGame/Font.cpp
+++ StrategyGame/Font.cpp
@@ -90,5 +90,46 @@ Font::Font(const char* fontPath) {
 }
 
 void Font::render(std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color, glm::mat4 projection) {
+    // Activate corresponding render state
+    this->shader.use();
     
+    this->shader.uniform3f("textColor", glm::vec3(color.x, color.y, color.z));
+    
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
+    
+    // Iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        Character ch = characters[*c];
+        
+        GLfloat xpos = x + ch.bearing.x * scale;
+        GLfloat ypos = y - (ch.size.y - ch.bearing.y) * scale;
+        
+        GLfloat w = ch.size.x * scale;
+        GLfloat h = ch.size.y * scale;
+        // Update VBO for each character
+        GLfloat vertices[6][4] = {
+            { xpos,     ypos + h,   0.0, 0.0 },
+            { xpos,     ypos,       0.0, 1.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+            
+            { xpos,     ypos + h,   0.0, 0.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+            { xpos + w, ypos + h,   1.0, 0.0 }
+        };
+        // Render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.textureID);
+        // Update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // Render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64)
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
