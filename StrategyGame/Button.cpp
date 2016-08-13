0ca72db404e12dab43813174f2257ffc3c3c6987
ja-San <jake_sanders@horacemann.org>
2016-08-13 16:23:00 -0400

    Text scales to fit in buttons

--- StrategyGame/Button.cpp
+++ StrategyGame/Button.cpp
@@ -77,18 +77,47 @@ void Button::render(bool mouseDown, bool mouseUp) {
     glDrawArrays(GL_TRIANGLES, 0, 6);
     glBindVertexArray(0);
     
+    //Set the scale for the text
+    GLfloat scale = 1.0f;
+    
     //Get the text size
-    glm::vec2 textSize = this->font.getSize(text, 1.0);
+    glm::vec2 textSize = this->font.getSize(text, scale);
     
     //Initialized with the lower left corner position of the button
+    
     //The arithmatic done on the lower left corner converts it from -1 to 1 into 0 to 1
     glm::vec2 textPos = glm::vec2((this->lowerLeftX + 1.0) / 2.0 * this->interfaceBoxWidth, (this->lowerLeftY + 1.0) / 2.0 * this->interfaceBoxHeight);
     
-    //Lower the text so its center is at the button's center
-    textPos.y += (this->buttonHeight * this->interfaceBoxHeight * 0.25) - (textSize.y / 2.0);
+    //The first .5 puts the button width/height back to 0 to 1 (instead of -1 to 1)
+    //The second .5 represents moving half of the button width/height rather than the full width/height
+    
+    //Horizontally center the text so its center is at the button's center
+    textPos.x += ((this->buttonWidth * 0.5) * this->interfaceBoxWidth) * 0.5 - (textSize.x / 2.0);
+    
+    //Vertically center the text so its center is at the button's center
+    textPos.y += ((this->buttonHeight * 0.5) * this->interfaceBoxHeight) * 0.5 - (textSize.y / 2.0);
+    
+    GLfloat sizeFraction = textSize.x / ((this->buttonWidth * 0.5) * this->interfaceBoxWidth); //This represents what fraction of the button the text will take up
+    
+    if (sizeFraction > 1) { //If the width of the text is greater than the button size, scale down the text proportionally
+        scale = 1.0 / sizeFraction;
+        
+        //Redo the size calculations with the new text size
+        
+        //Get the text size
+        textSize = this->font.getSize(text, scale);
+        
+        textPos = glm::vec2((this->lowerLeftX + 1.0) / 2.0 * this->interfaceBoxWidth, (this->lowerLeftY + 1.0) / 2.0 * this->interfaceBoxHeight);
+        
+        //Horizontally center the text so its center is at the button's center
+        textPos.x += ((this->buttonWidth * 0.5) * this->interfaceBoxWidth) * 0.5 - (textSize.x / 2.0);
+        
+        //Vertically center the text so its center is at the button's center
+        textPos.y += ((this->buttonHeight * 0.5) * this->interfaceBoxHeight) * 0.5 - (textSize.y / 2.0);
+    }
     
     //Render the text on the button
-    this->font.render(text, textPos.x, textPos.y, 1.0, glm::vec3(1.0f, 1.0f, 1.0f), this->interfaceBoxWidth, this->interfaceBoxHeight);
+    this->font.render(text, textPos.x, textPos.y, scale, glm::vec3(1.0f, 1.0f, 1.0f), this->interfaceBoxWidth, this->interfaceBoxHeight);
 }
 
 bool Button::isPressed() {
