15362c24204b7b3ed79f55298abf550e629cbc07
ja-San <jake_sanders@horacemann.org>
2016-08-28 13:43:59 -0400

    Button text added too

--- StrategyGame/Building.cpp
+++ StrategyGame/Building.cpp
@@ -10,7 +10,7 @@
 
 //Constructor
 
-Building::Building(unsigned int x, unsigned int y, std::string action, unsigned int maxHealth, unsigned int cost, unsigned int controller) : buildingX(x), buildingY(y), buildingAction(action), buildingMaxHealth(maxHealth), buildingCost(cost), buildingController(controller) {
+Building::Building(unsigned int x, unsigned int y, std::string buttonText, std::string action, unsigned int maxHealth, unsigned int cost, unsigned int controller) : buildingX(x), buildingY(y), buildingButtonText(buttonText), buildingAction(action), buildingMaxHealth(maxHealth), buildingCost(cost), buildingController(controller) {
     this->buildingHealth = this->buildingMaxHealth;
 }
 
@@ -24,6 +24,10 @@ bool Building::takeDamage(unsigned int damage) {
 }
 
 //Get functions
+
+const std::string Building::buttonText() {
+    return this->buildingButtonText;
+}
 
 const std::string Building::action() {
     return this->buildingAction;
--- StrategyGame/Building.hpp
+++ StrategyGame/Building.hpp
@@ -18,7 +18,7 @@ class Building {
 public:
     //Constructor
     
-    Building(unsigned int x, unsigned int y, std::string action, unsigned int maxHealth, unsigned int cost, unsigned int controller);
+    Building(unsigned int x, unsigned int y, std::string buttonText, std::string action, unsigned int maxHealth, unsigned int cost, unsigned int controller);
     
     //Destructor
     
@@ -36,6 +36,11 @@ public:
     bool takeDamage(unsigned int damage);
     
     //Public get functions
+    
+    /*!
+     * @return The text string for the button on the building to be used on the building interface.
+     */
+    const std::string buttonText();
     
     /*!
      * @return The action string for the button on the building to be used on the building interface.
@@ -74,6 +79,7 @@ public:
     
 private:
     //Private properties
+    const std::string buildingButtonText;
     const std::string buildingAction;
     const unsigned int buildingMaxHealth;
     const unsigned int buildingCost;
