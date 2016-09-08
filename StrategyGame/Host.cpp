9062e81e540f039456f7224a7417fb25d74619c5
ja-San <jake_sanders@horacemann.org>
2016-09-08 07:57:56 -0400

    Removed overlap between Game and Visualizer, started Host
    getPath() was in both Game file and Visualizer file

---
+++ StrategyGame/Host.cpp
@@ -0,0 +1,9 @@
+//
+//  Host.cpp
+//  Strategy Game
+//
+//  Created by Jake Sanders on 9/8/16.
+//  Copyright © 2016 Jake Sanders. All rights reserved.
+//
+
+#include "Host.hpp"
---
+++ StrategyGame/Host.hpp
@@ -0,0 +1,27 @@
+//
+//  Host.hpp
+//  Strategy Game
+//
+//  Created by Jake Sanders on 9/8/16.
+//  Copyright © 2016 Jake Sanders. All rights reserved.
+//
+
+#ifndef Host_hpp
+#define Host_hpp
+
+//Standard library includes
+#include <vector>
+
+//Local includes
+#include "Player.hpp"
+
+class Host {
+public:
+    
+private:
+    
+    std::vector<Player> players;
+    
+};
+
+#endif /* Host_hpp */
